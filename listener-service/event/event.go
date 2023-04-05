011340a37a0b676aed6dd8ab70cb784c797b115c
zohaibAsif <mushte.khak001@gmail.com>
2023-04-05 12:58:46 +0500

    added listener service

---
+++ listener-service/event/event.go
@@ -0,0 +1,28 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func declareExchange(ch *amqp.Channel) error {
+	return ch.ExchangeDeclare(
+		"logs_topic", // name
+		"topic",      // type
+		true,         // durable?
+		false,        // auto-deleted?
+		false,        // internal? false, because this exchange will be used between microservices
+		false,        // no-wait?
+		nil,          // arguments?
+	)
+}
+
+func declareRandomQueue(ch *amqp.Channel) (amqp.Queue, error) {
+	return ch.QueueDeclare(
+		"random_queue", // name
+		false,          // durable?
+		false,          // delete when unused?
+		true,           // exclusive?
+		false,          // no-wait?
+		nil,            // arguments?
+	)
+}
